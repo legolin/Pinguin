fc8f8618672a8e5757248b926e88d881bc282df9
Adam Elhardt <adam.elhardt@mangolanguages.com>
2011-09-20 20:17:49 -0400

    Tweaks to pinguin.rb

--- lib/pinguin.rb
+++ lib/pinguin.rb
@@ -6,14 +6,16 @@ ENV['RACK_ENV'] ||= 'development'
 
 BASEDIR = File.join(File.dirname(__FILE__), '..')
 
+Daemons.daemonize :dir_mode => :system if ARGV.include?('-d')
+
 mongoid_config_file = File.join(BASEDIR, 'config', 'mongoid.yml')
 Mongoid.load!(mongoid_config_file)
 
 Dir.chdir(File.join(BASEDIR, 'lib'))
 
-require './pinguin/host'
-require './pinguin/request'
-require './pinguin/penguin'
+require 'pinguin/host'
+require 'pinguin/request'
+require 'pinguin/penguin'
 
 config_file = File.join(BASEDIR, 'config', 'pinguin.yml')
 monitor = Pinguin::Penguin.new(config_file)
